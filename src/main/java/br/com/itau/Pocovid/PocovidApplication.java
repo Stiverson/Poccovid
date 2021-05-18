591514720b1b715fed836a55e36e688c2b7f9aa9
smachado <stiverson.machado@gmail.com>
2021-05-18 16:51:19 -0300

    Iniciando o projeto .

---
+++ src/main/java/br/com/itau/Pocovid/PocovidApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.Pocovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocovidApplication.class, args);
+	}
+
+}
