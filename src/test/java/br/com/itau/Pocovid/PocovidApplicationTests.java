f87e6b2fce028bac4c2c7ec87e2a6b2b43c9577f
wendersonnf <wendersonnsantiago@gmail.com>
2021-05-19 12:17:33 -0300

    Resolvendo dependencia: hibernate-entitymanager

--- src/test/java/br/com/itau/Pocovid/PocovidApplicationTests.java
+++ src/test/java/br/com/itau/Pocovid/PocovidApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.itau.Pocovid;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PocovidApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.itau.Pocovid;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PocovidApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
