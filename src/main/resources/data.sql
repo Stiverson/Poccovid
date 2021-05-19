39431070df71d2e94eea26202969d96df5e69936
fabriciond <fabriciond@ciandt.com>
2021-05-19 10:09:25 -0300

    config do data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO CASES_BY_STATE(uid, uf, state, cases, deaths, suspects, refuses, datetime) VALUES(11, 'Rondonia', 'RO', 0, 0, 61, 2, '2020-03-18T23:00:00.000Z');
+INSERT INTO CASES_BY_STATE(uid, uf, state, cases, deaths, suspects, refuses, datetime) VALUES(11, 'São Paulo', 'SP', 0, 0, 220, 12, '2020-04-18T23:00:00.000Z');
+
+INSERT INTO CASES_BY_STATE_AND_DATE(uid, uf, state, cases, deaths, suspects, refuses, datetime) VALUES(11, 'São Paulo', 'SP', 0, 0, 220, 12, '2020-03-18T23:00:00.000Z');
+
+
+INSERT INTO CASES_BY_COUNTRY(coutry, cases, confirmed, deaths, recovered, updated_at) VALUES('Canada', 1299, 1328, 19, 10, '2020-03-21T23:43:02.000Z');
