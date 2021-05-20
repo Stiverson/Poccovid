b8778cd7af8945e731076892e4a888fef2c783fc
fabriciond <fabriciond@ciandt.com>
2021-05-20 15:08:11 -0300

    criação de converters de DTO

--- src/main/java/br/com/itau/Pocovid/service/PocovidService.java
+++ src/main/java/br/com/itau/Pocovid/service/PocovidService.java
@@ -1,7 +1,28 @@
 package br.com.itau.Pocovid.service;
 
+import br.com.itau.Pocovid.model.CountryDto;
+import br.com.itau.Pocovid.model.StateDto;
+import br.com.itau.Pocovid.repository.CountryRepository;
+import br.com.itau.Pocovid.repository.StatesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PocovidService {
+
+    @Autowired
+    private StatesRepository statesRepository;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<StateDto> getStateList() {
+       return StateDto.converter(statesRepository.findAll());
+    }
+
+    public List<CountryDto> getCountryList() {
+        return CountryDto.converter(countryRepository.findAll());
+    }
 }
