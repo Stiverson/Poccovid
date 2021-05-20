c6e041adaa3826efdedf0115555358a3cfff2410
fabriciond <fabriciond@ciandt.com>
2021-05-20 14:58:52 -0300

    Criação de converters do DTO

--- src/main/java/br/com/itau/Pocovid/model/CountryDto.java
+++ src/main/java/br/com/itau/Pocovid/model/CountryDto.java
@@ -1,7 +1,13 @@
 package br.com.itau.Pocovid.model;
 
+import br.com.itau.Pocovid.entites.Country;
+import br.com.itau.Pocovid.entites.State;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -13,4 +19,17 @@ public class CountryDto {
     private Integer deaths;
     private Integer recovered;
     private String updated_at;
+
+    public CountryDto(Country country) {
+        this.country = country.getCountry();
+        this.cases = country.getCases();
+        this.confirmed = country.getConfirmed();
+        this.deaths = country.getDeaths();
+        this.recovered = country.getRecovered();
+        this.updated_at = getUpdated_at();
+    }
+
+    public static List<CountryDto> converter(List<Country> states) {
+        return states.stream().map(CountryDto::new).collect(Collectors.toList());
+    }
 }
--- src/main/java/br/com/itau/Pocovid/model/StateDto.java
+++ src/main/java/br/com/itau/Pocovid/model/StateDto.java
@@ -1,8 +1,12 @@
 package br.com.itau.Pocovid.model;
 
 
+import br.com.itau.Pocovid.entites.State;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -16,4 +20,18 @@ public class StateDto {
     private Integer suspects;
     private Integer refuses;
     private String datetime;
+
+    public StateDto(State state) {
+        this.uf = state.getUf();
+        this.state = state.getState();
+        this.cases = state.getCases();
+        this.deaths = state.getDeaths();
+        this.suspects = state.getSuspects();
+        this.refuses = state.getRefuses();
+        this.datetime = state.getDatetime();
+    }
+
+    public static List<StateDto> converter(List<State> state) {
+        return state.stream().map(StateDto::new).collect(Collectors.toList());
+    }
 }
