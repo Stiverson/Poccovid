3c5e3dcf4b218007731ca054249c7c544177f82a
smachado <stiverson.machado@gmail.com>
2021-05-25 11:07:23 -0300

    Criação da classe de test state .

---
+++ src/test/java/br/com/itau/Pocovid/service/impl/StateServiceImplTest.java
@@ -0,0 +1,16 @@
+package br.com.itau.Pocovid.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateServiceImplTest {
+
+    @Test
+    void getStateList() {
+    }
+
+    @Test
+    void getStateByUfAndDate() {
+    }
+}
